b812a190c3dbbdff6d30ea59fc4ac633f4cbed36
Brent Johnson <xone47@yahoo.com>
2026-04-19 21:18:45 -0400

    Support variable players in testing startup script.

--- run.sh
+++ run.sh
@@ -2,10 +2,14 @@ cargo build --bin powergrid-server
 
 trap 'kill $(jobs -p) 2>/dev/null' EXIT
 
-cargo run --bin powergrid-client -- --name brent --color red --url ws://localhost:3000/ws &
-sleep 1
-cargo run --bin powergrid-client -- --name brad --color blue --url ws://localhost:3000/ws &
-sleep 1
-cargo run --bin powergrid-client -- --name nick --color green --url ws://localhost:3000/ws &
-sleep 1
+NUM_PLAYERS=${1:-3}
+
+PLAYERS=("brent:red" "brad:blue" "nick:green" "jamie:yellow" "niki:purple" "jodi:white")
+
+for i in $(seq 0 $((NUM_PLAYERS - 1))); do
+    IFS=':' read -r name color <<< "${PLAYERS[$i]}"
+    cargo run --bin powergrid-client -- --name "$name" --color "$color" --url ws://localhost:3000/ws &
+    sleep 1
+done
+
 RUST_LOG=info cargo run --bin powergrid-server
